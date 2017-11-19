d005d2ab31bac3e71fa2227a884d7f2cf83820d9
liangmoyanTheProgramer <1792070600@qq.com>
2017-11-19 17:16:24 +0800

    First commit with cleaning code

--- app/src/main/java/com/mystudio/moshui/inkforum/EntryActivity.java
+++ app/src/main/java/com/mystudio/moshui/inkforum/EntryActivity.java
@@ -19,13 +19,7 @@ public class EntryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
     @Override
